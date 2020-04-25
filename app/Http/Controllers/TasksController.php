7438cd1edc98224f0d4bc888d1878c76db96c7ea
tepin <tepin.study@gmail.com>
2020-04-25 15:59:52 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -47,10 +47,12 @@ class TasksController extends Controller
         //必須、191文字以内
         $this->validate($request, [
             'content' => 'required|max:191',
+            'status' => 'required|max:10',
             ]);
-            
+
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         
         return redirect('/');
@@ -94,8 +96,15 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        //必須、191文字以内
+        $this->validate($request, [
+            'content' => 'required|max:191',
+            'status' => 'required|max:10',
+            ]);
+
         $task = Task::find($id);
         $task->content = $request->content;
+        $task->status = $request->status;
         $task->save();
         
         return redirect('/');
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -10,6 +10,7 @@
                 <tr>
                     <th>id</th>
                     <th>メッセージ</th>
+                    <th>タスク</th>
                 </tr>
             </thead>
             <tbody>
@@ -17,6 +18,7 @@
                 <tr>
                     <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
+                    <td>{{ $task->status }}</td>
                 </tr>
                 @endforeach
             </tbody>
